d31b47d03f9b94afa53170b84c3221c3ef3d3988
EcoScore-Website <rullochristian@gmail.com>
2026-03-29 19:59:58 +0200

    Fix: fehlendes </div> in frw/[slug]/page.tsx (Build-Fehler)

--- src/app/frw/[slug]/page.tsx
+++ src/app/frw/[slug]/page.tsx
@@ -157,6 +157,7 @@ export default async function FrwChapterPage({ params, searchParams }: Props) {
           </Link>
         ))}
       </div>
+      </div>
 
       {/* Tab Content */}
       <div
